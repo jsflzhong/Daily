dbcd1ada29337e4b558f8204b84482f2d0f4ed60
username <jsflzhong@163.com>
2025-03-07 23:18:45 +0800

    Create algorithm 33: SearchInRotatedSortedArray

---
+++ src/Algorithm/al_33_Search in Rotated Sorted Array.java
@@ -0,0 +1,47 @@
+package Algorithm;
+
+class SearchInRotatedSortedArray {
+
+    public static void main(String[] args) {
+        int[] nums = {6,7,8,9,10,11,12,0,1,2,3,4,5};
+        
+        System.out.println (search(nums, 9));
+    }
+
+    /**
+     * Input: nums = [4,5,6,7,0,1,2], target = 0
+     * Output: 4
+     * 
+     * Input: nums = [6,7,8,9,10,11,12,0,1,2,3,4,5], target = 9
+     */
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+           if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+
+        }
+
+        return -1;
+    }
+
+
+}
